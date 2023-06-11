f2a78c47a6fdb194c7fdc07b1683ed2c24c2ebd1
Paulo3678 <pauloecomercial367@gmail.com>
2023-06-11 18:43:29 -0300

    :sparkles: Liskov Substitution Principle

--- src/Model/AluraMais.php
+++ src/Model/AluraMais.php
@@ -6,6 +6,17 @@ use Model\IPontuavel;
 
 class AluraMais extends Video implements IPontuavel
 {
+
+    /**
+     * Liskov Substitution Principle 
+     * Principio de Substituição de Liskov
+     * If it looks like a duck, quacks like a duck, but needs batteries - You probably have the wrong abstraction
+     * Se q(x) uma propriedade que se pode provar do objeto x do tipo T. Então, q(y) também é possível provar para o objeto y
+       do tipo S, sendo S um subtipo de T
+     * Classes filhas nunca deveriam infringir as definições do tipo da classe pai
+     * 
+     */
+    
     private $categoria;
 
     public function __construct(string $nome, string $categoria)
@@ -16,7 +27,7 @@ class AluraMais extends Video implements IPontuavel
 
     public function recuperarUrl(): string
     {
-        return str_replace(' ', '-', strtolower($this->categoria));
+        return 'http://videos.alura.com.br/' . str_replace(' ', '-', strtolower($this->categoria)) . $this->nome;
     }
 
     public function recuperarPontuacao(): int
